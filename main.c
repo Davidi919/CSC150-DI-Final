799071c004fbdb8071411c539caf2776240bf53c
David <david.inabinette@gmail.com>
2024-12-01 21:55:24 -0600

    add orbital period for days

--- main.c
+++ main.c
@@ -23,29 +23,26 @@ int main()
     {
         char name[10];
         double orbital_radius;
-        double orbital_period; 
+        double orbital_period;
+        double orbital_period_days;
     };
 
     const struct Planet planets[] = {
         // Orbital period in seconds
-        {"Mercury", 57910000 * 1000, 7600521.6},     // Orbital period: ~88 days
-        {"Venus", 108200000 * 1000, 19413907.2},     // Orbital period: ~225 days
-        {"Earth", 149600000 * 1000, 31557600.0},     // Orbital period: ~365.25 days
-        {"Mars", 227940000 * 1000, 59355072.0},      // Orbital period: ~687 days
-        {"Jupiter", 778330000 * 1000, 374335776.0},  // Orbital period: ~4331 days
-        {"Saturn", 1429400000 * 1000, 929596608.0},  // Orbital period: ~10,747 days
-        {"Uranus", 2870990000 * 1000, 2651370016.0}, // Orbital period: ~30,589 days
-        {"Neptune", 4504300000 * 1000, 5200418592.0} // Orbital period: ~59,800 days
-    };
+        {"Mercury", 57910000 * 1000, 7600521.6},
+        {"Venus", 108200000 * 1000, 19413907.2},
+        {"Earth", 149600000 * 1000, 31557600.0},
+        {"Mars", 227940000 * 1000, 59355072.0},
+        {"Jupiter", 778330000 * 1000, 374335776.0},
+        {"Saturn", 1429400000 * 1000, 929596608.0},
+        {"Uranus", 2870990000 * 1000, 2651370016.0},
+        {"Neptune", 4504300000 * 1000, 5200418592.0}};
 
     // user variables
     int start_planet;
     int destination_planet;
     double initial_mass;
     int rocket_type;
-
-    
-
 
     //---------------------------------------------
     /*begin program*/
